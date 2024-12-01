ed43bd70837d767fb1d7e0ba5ab5a9c3fd43683b
KruboKennedy <kennedykrubob@gmail.com>
2024-12-01 23:44:25 +0000

    i added picture to the index and about me page to the contact page to the footer i added background-color

---
+++ hello.js
@@ -0,0 +1,25 @@
+<!DOCTYPE html>
+<html lang="en">
+    <head>
+    </head>
+    <body>
+        <h1 class="heading" id ="head">Hello World.</h1>
+        <script>
+      (document.querySelector("#heading"))
+            
+        </script>
+    </body>
+
+
+
+
+
+
+
+
+
+
+
+
+
+</html>
---
+++ script.js
@@ -0,0 +1,13 @@
+//Modal for proflie photo
+const profliephoto = document.getElementById('proflie-photo');
+const photoModal = document.getElementById('photp-modal');
+const closeModal = document.getElementById(closed-modal);
+
+profliephoto.addEventListener('click,'()=>{
+    photoModal.style.display = 'flex'
+
+});
+closedModal.addEventListener('click,'()=>{
+    photoModal.style.display ='none';
+    
+});
